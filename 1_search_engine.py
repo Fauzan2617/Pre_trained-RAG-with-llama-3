f6f52c976a133881da3fa6fae636514ea4527887
Fauzan2617 <mfauzandwiputera10@gmail.com>
2026-04-08 23:31:03 +0700

    Chat History 100%

--- 1_search_engine.py
+++ 1_search_engine.py
@@ -24,3 +24,41 @@ documents = loader.load()
 # 2. chunking (memecah teks jadi bgaian kecil)
 #===========================
 
+print ("Memecah dokument menjadi chunks")
+
+# Membuat objek text splitter
+text_splitter = RecursiveCharacterTextSplitter (
+    chunk_size = 200, # Maksimal 200 karakter per potongan
+    chunk_overlap = 50 # setiap potongan overlap 50 karakter agar konteks tidak hilang
+)
+
+# Fungsi memecah document menjadi beberapa bagian
+chunks = text_splitter.split_documents(documents)
+
+#========================
+# 3. Embedding (Ubah teks jadi angka)
+#========================
+
+print ("Mengunduh/Memuat model embedding Hungging Face...")
+
+# Menggunakan model embedding gratis dari hungging face
+# Model ini akan mengubah teks menjadi vektor
+embeddings = HuggingFaceEmbeddings (
+    model_name = "all-MiniLM-L6-v2" # model ringan dan cepat hanya untuk embedding
+)
+
+#=====================
+# 4. Simpan Ke Vector Database
+#=====================
+
+print ("Menyimpan ke vector Database...")
+
+# Membuat vector database menggunakan Chroma 
+vectorstore = Chroma.from_documents(
+    documents = chunks,             # Data yang dipecah    
+    embedding = embeddings,         # Model untuk mengubah teks menjadi vektor 
+    persist_directory = "./db"      # Lokasi penyimpanan database
+)
+
+# Menampilkan log bahwa proses selesai
+print ("Selesai Database vektor berhasil dibuat di folder '/db'.")
